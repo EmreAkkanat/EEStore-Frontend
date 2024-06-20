8db0c4c3d708d58d29bad006d958868e5616992d
emrea <emreakka01@gmail.com>
2024-06-21 00:04:16 +0300

    add full screen dialog component

--- src/admin/pages/category/CategoryPage.jsx
+++ src/admin/pages/category/CategoryPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Table from '../../components/data-table/DataTable';
+import FullScreenDialog from '../../components/dialog/FullScreenDialog';
 
-const CategoryPage = () => {
+const CategoryPage = () => {  
   return (
-    <>
+    <React.Fragment>
+      <FullScreenDialog/>
       <Table />
-    </>
+    </React.Fragment>
   )
 }
 
